88c9ed4ba1c689496000e4ed99b377b820b66db8
El Mahdi Affaoui <mahdi.affaoui@gmail.com>
2025-11-01 15:14:20 +0100

    Started to use Angular Material Fixed bugs on delete list

--- task-manager-angular-client/src/app/tasks-viewer/tasks-viewer.ts
+++ task-manager-angular-client/src/app/tasks-viewer/tasks-viewer.ts
@@ -2,10 +2,13 @@ import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import { DataApi, List} from '../data-api/data-api';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ListViewer } from '../list-viewer/list-viewer';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'tasks-viewer',
-  imports: [ReactiveFormsModule, FormsModule, ListViewer],
+  imports: [ReactiveFormsModule, FormsModule, ListViewer, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './tasks-viewer.html'
 })
 export class TasksViewer {
@@ -47,7 +50,8 @@ export class TasksViewer {
   deleteList(listId: string){
     this.dataApi.deleteList(listId).subscribe((response) => {
       this.lists = this.lists.filter(list => list.id !== listId);
-      this.changeDetector.detectChanges();
+      if(this.currentlyOpen === listId) this.currentlyOpen = "";
+      this.changeDetector.markForCheck();
     });
   }
 
